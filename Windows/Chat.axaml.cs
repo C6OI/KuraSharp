5dfb8df83e5b289fba450bf930e0e3b79c656d88
C6OI <C6OI@yandex.ru>
2022-10-08 04:23:57 +0500

    Simple server switching and channels creating (very bugged but i wanna sleep)

--- Windows/Chat.axaml.cs
+++ Windows/Chat.axaml.cs
@@ -1,17 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Layout;
+using Avalonia.Markup.Xaml.Templates;
 using Avalonia.Media;
 using Avalonia.Media.Imaging;
 using Avalonia.Threading;
 using KuraSharp.Data;
 using KuraSharp.Extensions;
+using MessageBox.Avalonia;
+using MessageBox.Avalonia.BaseWindows.Base;
+using MessageBox.Avalonia.DTO;
+using MessageBox.Avalonia.Views;
 using Newtonsoft.Json;
 using Serilog;
 
@@ -19,26 +23,42 @@ namespace KuraSharp.Windows;
 
 public partial class Chat : Window {
     static readonly ILogger Logger = Log.Logger.ForType<Chat>();
-    static readonly UserData UserData = StaticData.UserData;
+    static readonly UserData UserData = StaticData.UserData!;
+    Dictionary<string, string> _headers = new() { { "Authorization", UserData.Token } };
 
     public Chat() {
         InitializeComponent();
         ClientSize = StaticData.Size;
         
-        FetchServers(true);
+        // Don't remove it! Previewer doesn't work without it
+        if (StaticData.UserData == null) return;
+        
+        SetupProfile();
 
 #if DEBUG
         this.AttachDevTools();
 #endif
     }
+
+    async void SetupProfile() {
+        Avatar.Source = new Bitmap(await UrlExtensions.GetPictureAsStream(UserData.Avatar));
+        Username.Text = $"{UserData.Username}#{UserData.Discriminator}";
+
+        FetchServers(true);
+    }
+
+    async void OpenChannel(Channel channel) {
+        Logger.Information($"Trying to open channel {channel.Name}");
+    }
+
+    async void OpenServer(GuildData server) {
+        ServerInfo.Header = server.Name;
+        CreateChannel.Click += (s, e) => CreateChannelClick(s, e, server);
+    }
     
     async void FetchServers(bool addToPanel) {
-        Dictionary<string, string> headers = new() {
-            { "Authorization", UserData.Token }
-        };
-
         await Parallel.ForEachAsync(UserData.Guilds, async (guild, cancel) => {
-            BaseData response = await UrlExtensions.JsonHttpRequest($"https://api.kuracord.tk/guilds/{guild.Guild.Id}/", HttpMethod.Get, null, headers);
+            BaseData response = await UrlExtensions.JsonHttpRequest($"https://api.kuracord.tk/guilds/{guild.Guild.Id}/", HttpMethod.Get, null, _headers);
             string jsonResponse = await response.Data.ReadAsStringAsync(cancel);
 
             if (!response.IsOk) {
@@ -65,34 +85,68 @@ public partial class Chat : Window {
             Width = 70, Height = 70,
             CornerRadius = new CornerRadius(50),
             HorizontalContentAlignment = HorizontalAlignment.Center,
-            VerticalContentAlignment = VerticalAlignment.Center
+            VerticalContentAlignment = VerticalAlignment.Center,
+            ClickMode = ClickMode.Release
         };
 
         if (guild.Icon != null) {
             Image serverIcon = new() {
-                Source = new Bitmap(await UrlExtensions.GetPictureAsStream(guild.Icon)),
-                Stretch = Stretch.Uniform
+                Source = new Bitmap(await UrlExtensions.GetPictureAsStream(guild.Icon))
             };
             
             server.Content = serverIcon;
         }
 
+        server.Click += (s, e) => {
+            CreateChannel.Click -= (se, ev) => CreateChannelClick(se, ev, guild); 
+            OpenServer(guild);
+        };
+
         Servers.Children.Add(server);
     }
+    
+    async void CreateChannelClick(object? s, RoutedEventArgs e, GuildData server) {
+        Dictionary<string, string> json = new();
+        MessageBoxInputParams mBoxInput = new() {
+            WindowIcon = Icon,
+            ShowInCenter = true,
+            WindowStartupLocation = WindowStartupLocation.CenterScreen,
+            Width = 480, Height = 240,
+            Icon = MessageBox.Avalonia.Enums.Icon.Plus,
+            Multiline = false
+        };
+        
+        IMsBoxWindow<MessageWindowResultDTO> messageBoxInputWindow = MessageBoxManager.GetMessageBoxInputWindow(mBoxInput);
+        MessageWindowResultDTO messageWindowResultDto = await messageBoxInputWindow.ShowDialog(this);
 
-    async void Upload_OnClick(object? s, RoutedEventArgs e) {
-        byte[] bytes = await File.ReadAllBytesAsync(@"C:\Users\Stm07\Desktop\nado\DitGu-hSxYM.jpg");
-        string picture = Convert.ToBase64String(bytes);
+        if (messageWindowResultDto.Button != "Confirm") return;
 
-        Dictionary<string, string> data = new() {
-            { "name", "ХУЙ" },
-            { "file", picture }
+        json.Add("name", messageWindowResultDto.Message);
+        BaseData response = await UrlExtensions.JsonHttpRequest($"https://api.kuracord.tk/guilds/{server.Id}/channels", HttpMethod.Post, json, _headers);
+
+        if (!response.IsOk) {
+            ErrorData error = JsonConvert.DeserializeObject<ErrorData>(await response.Data.ReadAsStringAsync());
+            Logger.Error(error.Message);
+            return;
+        }
+
+        Channel channel = JsonConvert.DeserializeObject<Channel>(await response.Data.ReadAsStringAsync());
+        Button channelButton = new() {
+            Name = channel.Name,
+            Tag = channel.Id,
+            Content = channel.Name,
+            BorderThickness = new Thickness(0),
+            Background = new SolidColorBrush(Colors.Transparent),
+            BorderBrush = new SolidColorBrush(Colors.Transparent),
+            HorizontalContentAlignment = HorizontalAlignment.Left,
+            VerticalContentAlignment = VerticalAlignment.Center,
+            ClickMode = ClickMode.Release
         };
 
-        Dictionary<string, string> headers = new() {
-            { "Authorization", UserData.Token }
-        };
+        channelButton.Click += (se, ev) => OpenChannel(channel);
 
-        await UrlExtensions.JsonHttpRequest("https://api.kuracord.tk/guilds/3/", HttpMethod.Patch, data, headers);
+        Channels.Children.Add(channelButton);
+
+        Logger.Information($"Created channel {channel.Name}");
     }
 }
